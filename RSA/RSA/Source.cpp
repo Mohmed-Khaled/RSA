d7760a4d4862c3579bb9de614313173681466745
Mohamed_Khaled <mr_tlmk@hotmail.com>
2016-12-20 16:22:23 +0200

    Quick Fixes but all operations needs major enhancement

--- RSA/RSA/Source.cpp
+++ RSA/RSA/Source.cpp
@@ -3,7 +3,6 @@
 #include <random>
 #include <cmath>
 #include <ctime>
-#include <vector>
 
 using namespace std;
 
@@ -12,7 +11,7 @@ class LongNumber
 public:
 	LongNumber();
 	LongNumber(string n_str);
-	LongNumber(int n);
+	LongNumber(long long n);
 	LongNumber operator*(const LongNumber& b);
 	LongNumber operator/(const LongNumber& b);
 	LongNumber operator%(const LongNumber& b);
@@ -26,27 +25,30 @@ public:
 	LongNumber operator-(const LongNumber& b);
 	LongNumber ModularExponentiation(const LongNumber& power, const LongNumber& mod);
 	LongNumber ModularInverse(const LongNumber& mod);
+	LongNumber Exponentiation(const LongNumber& power);
+	LongNumber MulExponentent10(int power);
+	LongNumber DivExponentent10(int power);
+	LongNumber ModExponentent10(int power);
 	LongNumber Absolute();
-	bool IsPrime();
+	bool IsPrime(int iterations);
 	bool IsEven();
 	bool IsOdd();
 	string getStringNumber();
 private:
 	bool sign; //true means positive and false means negative
 	string number;
-	bool MillerRabinTest(LongNumber n, LongNumber q, int k);
+	bool MillerRabinTest(LongNumber& n,LongNumber& q, int k);
 	void RemoveLeadingZeros();
 	LongNumber DivideBy2();
+	LongNumber static karatsuba(LongNumber& a,LongNumber& b);
 };
 
-string Encrypt(const string& message, const LongNumber& number, const LongNumber& mod);
-
-void test_addition_time(int n) 
+void test_addition_time(int n)
 {
 	LongNumber x("12369571528747655798110188786567180759626910465726920556567298659370399748072366507234899432827475865189642714067836207300153035059472237275816384410077871");
 	LongNumber y("2065420353441994803054315079370635087865508423962173447811880044936318158815802774220405304957787464676771309034463560633713497474362222775683960029689473");
 	clock_t begin = clock();
-	for(int i = 0;i < n;i++)
+	for (int i = 0; i < n; i++)
 	{
 		LongNumber z = x + y;
 	}
@@ -87,24 +89,24 @@ void test_division_time(int n)
 	clock_t begin = clock();
 	for (int i = 0; i < n; i++)
 	{
-		LongNumber z = x / 2;
+		LongNumber z = x / y;
 	}
 	clock_t end = clock();
 	cout << "Divisiom Time = " << double(end - begin) / CLOCKS_PER_SEC << endl << endl;
 }
 
-void test_prime_check_time(string p_str)
+void test_prime_check_time(string p_str, int i)
 {
 	LongNumber x(p_str);
 	clock_t begin = clock();
-	bool check = x.IsPrime();
+	bool check = x.IsPrime(i);
 	clock_t end = clock();
-	cout << "Prime Check Time for " << p_str << " = " 
+	cout << "Prime Check Time for " << p_str << " = "
 		<< double(end - begin) / CLOCKS_PER_SEC << endl;
-	cout << ((check) ? "Prime" : "Not Prime")  << endl << endl;
+	cout << ((check) ? "Prime" : "Not Prime") << endl << endl;
 }
 
-void test_modular_exponentiation_time(string m_str, string e_str,string n_str)
+void test_modular_exponentiation_time(string m_str, string e_str, string n_str)
 {
 	LongNumber m(m_str);
 	LongNumber e(e_str);
@@ -112,29 +114,34 @@ void test_modular_exponentiation_time(string m_str, string e_str,string n_str)
 	clock_t begin = clock();
 	LongNumber r = m.ModularExponentiation(e, n);
 	clock_t end = clock();
-	cout << "Modular Exponentiation Time for M = " << m_str 
-		<< " and e = " << e_str 
+	cout << "Modular Exponentiation Time for M = " << m_str
+		<< " and e = " << e_str
 		<< " and n = " << n_str << " : "
 		<< double(end - begin) / CLOCKS_PER_SEC << endl;
 	cout << "Result = " << r.getStringNumber() << endl << endl;
 }
 
+string Encrypt(const string& message, const LongNumber& number, const LongNumber& mod);
+
 int main()
 {
 	// TEST START
-	int test_itr = 10000;
-	//test_addition_time(test_itr);
-	//test_subtraction_time(test_itr);
-	//test_multiplication_time(test_itr);
-	//test_division_time(test_itr);
-	string p_str = "12369571528747655798110188786567180759626910465726920556567298659370399748072366507234899432827475865189642714067836207300153035059472237275816384410077871";
-	//test_prime_check_time(p_str);
+	int test_itr = 100;
+	test_addition_time(test_itr);
+	test_subtraction_time(test_itr);
+	test_multiplication_time(test_itr);
+	test_division_time(test_itr);
 	string m_str = "88";
 	string c_str = "4397678428390379126255360246165021910057442267382175543246817108158797115317154540746718616555865161372450860559307149988169566508274711121236049281217144195628407516579953387138808449458611836421032431582081899650685651973204503916459595600207918950383877057152533041873901965623112895996177941667469292738";
 	string e_str = "65537";
+	string d_str = "25051719899710729069339146813050963409059898810366373119834423967819636191509401691818253978210229371822961344590338934536803264841097247978074700319812702399440521918349189245279566231685265955731649745935378380489722580113725907099133943430294137060596724659637599737926649148356615085679203385772673944833";
 	string n_str = "25548364798832019218170326077010425733930233389897468141147917831084690989884562791601588954296621731652139141347541240725432606132471100644835778517336041031200174441223836394229943651678525471050219216183727749114047330431603023948126844573697946795476319956787513765533596926704755530772983549787878951983";
 	test_modular_exponentiation_time(m_str, e_str, n_str);
-	test_modular_exponentiation_time(c_str, e_str, n_str);
+	test_modular_exponentiation_time(c_str, d_str, n_str);
+	string p_str = "12369571528747655798110188786567180759626910465726920556567298659370399748072366507234899432827475865189642714067836207300153035059472237275816384410077871";
+	string q_str = "2065420353441994803054315079370635087865508423962173447811880044936318158815802774220405304957787464676771309034463560633713497474362222775683960029689473";
+	test_prime_check_time(p_str, 1);
+	test_prime_check_time(q_str, 1);
 	// TEST END*/
 	string P_str, Q_str, E_str;
 	P_str = "P=12369571528747655798110188786567180759626910465726920556567298659370399748072366507234899432827475865189642714067836207300153035059472237275816384410077871";
@@ -161,12 +168,12 @@ int main()
 		if (operation == "Quit") break;
 		if (operation == "IsPPrime")
 		{
-			string response = (P.IsPrime()) ? "Yes" : "No";
+			string response = (P.IsPrime(1)) ? "Yes" : "No";
 			cout << response << endl;
 		}
 		else if (operation == "IsQPrime")
 		{
-			string response = (Q.IsPrime()) ? "Yes" : "No";
+			string response = (Q.IsPrime(1)) ? "Yes" : "No";
 			cout << response << endl;
 		}
 		else if (operation == "PrintN")
@@ -225,7 +232,7 @@ LongNumber::LongNumber(string n_str)
 	RemoveLeadingZeros();
 }
 
-LongNumber::LongNumber(int n)
+LongNumber::LongNumber(long long n)
 {
 	if (n < 0)
 	{
@@ -242,33 +249,59 @@ LongNumber::LongNumber(int n)
 
 LongNumber LongNumber::operator*(const LongNumber & b)
 {
-	string n1 = number;
-	string n2 = b.number;
-	LongNumber mul;
-	if (n1.length() > n2.length())
-		n1.swap(n2); //to decrease calls to add operation
-	for (int i = n1.size() - 1; i >= 0; i--)
+	//TODO: Try to enhance Muyliplication.
+	if (number == "0" || b.number == "0")
 	{
-		string tmp = n2;
-		unsigned int d1 = n1[i] - '0';
-		unsigned int c = 0; //carry
-		for(int j = tmp.size() - 1; j >= 0;j--)
+		return LongNumber(0);
+	}
+	if (number == "1")
+	{
+		return b;
+	}
+	if (b.number == "1")
+	{
+		return *this;
+	}
+	LongNumber mul;
+	if (number.size() < 10 && b.number.size() < 10)
+	{
+		mul = stoll(number) * stoll(b.number);
+	}
+	else if(number.size() < 80 || b.number.size() < 80)
+	{
+		string n1 = number;
+		string n2 = b.number;
+		if (n1.length() > n2.length())
+			n1.swap(n2); //to decrease calls to add operation
+		for (int i = n1.size() - 1; i >= 0; i--)
 		{
-			unsigned int d2 = tmp[j] - '0';
-			d2 = d1 * d2 + c;
-			if (d2 > 9)
+			string tmp = n2;
+			unsigned int d1 = n1[i] - '0';
+			unsigned int c = 0; //carry
+			for (int j = tmp.size() - 1; j >= 0; j--)
 			{
-				c = d2 / 10;
-				d2 -= c * 10;
+				unsigned int d2 = tmp[j] - '0';
+				d2 = d1 * d2 + c;
+				if (d2 > 9)
+				{
+					c = d2 / 10;
+					d2 -= c * 10;
+				}
+				else
+					c = 0;
+				tmp[j] = d2 + '0';
 			}
-			else
-				c = 0;
-			tmp[j] = d2 + '0';
+			if (c > 0)
+				tmp.insert(0, 1, c + '0');
+			tmp.append(n1.size() - 1 - i, '0');//result of multiplication with tens, hundreds,...
+			mul = mul + tmp;
 		}
-		if (c > 0)
-			tmp.insert(0, 1, c + '0');
-		tmp.append(n1.size() - 1 - i, '0');//result of multiplication with tens, hundreds,...
-		mul = mul + tmp;
+	}
+	else
+	{
+		LongNumber op1 = *this;
+		LongNumber op2 = b;
+		mul = karatsuba(op1, op2);
 	}
 	RemoveLeadingZeros();
 	mul.sign = !(sign ^ b.sign);
@@ -277,7 +310,7 @@ LongNumber LongNumber::operator*(const LongNumber & b)
 
 LongNumber LongNumber::operator/(const LongNumber & b)
 {
-	//TODO: Try to enhance division algorithm.
+	//TODO: Try to enhance Division.
 	if (number == "0" || (*this < b))
 		return LongNumber();
 	if (b.number == "1")
@@ -379,7 +412,6 @@ LongNumber LongNumber::operator/(const LongNumber & b)
 
 LongNumber LongNumber::operator%(const LongNumber & b)
 {
-	//TODO: Try to enhance mod operation.
 	// not tested with negative numbers as it is not needed in RSA
 	if (number == "0" || *this == b)
 		return LongNumber();
@@ -509,6 +541,10 @@ bool LongNumber::operator>=(const LongNumber & b)
 
 LongNumber LongNumber::operator+(const LongNumber & b)
 {
+	if (number == "0")
+		return b;
+	if (b.number == "0")
+		return *this;
 	if(sign && !b.sign)
 	{
 		LongNumber tmp = b;
@@ -603,7 +639,8 @@ LongNumber LongNumber::operator-(const LongNumber & b)
 		if (n1[i] < n2[i])
 		{
 			n1[i] += 10;
-			n1[i - 1]--;
+			if(i != 0)
+				n1[i - 1]--;
 		}
 		sub[i] = ((n1[i] - '0') - (n2[i] - '0')) + '0';
 	}
@@ -614,41 +651,46 @@ LongNumber LongNumber::operator-(const LongNumber & b)
 
 LongNumber LongNumber::ModularExponentiation(const LongNumber & power, const LongNumber & mod)
 {
-	//TODO: Try to enhance ModularExponentiation 
+	//TODO: Try to enhance ModularExponentiation. 
 	if (mod.number == "1")
 		return LongNumber();
 	LongNumber base = *this;
-	LongNumber p = power;
-	LongNumber result = 1;
 	base = base % mod;
-	//algo start
-	/*while (p > 0)
-	{
-		if (p.IsOdd())
-			result = (result * base) % mod;
-		p = p / 2;
-		base = (base * base) % mod;
-	}*/
+	LongNumber p = power;
 	if (p == 1) {
 		return base;
 	}
 	if (p == 0) {
 		return LongNumber(1);
 	}
-	LongNumber m = base.ModularExponentiation(p / 2, mod);
-	LongNumber res;
-	if (p.IsEven()) 
+	if(p.number.size() > 300)
 	{
-		res = LongNumber(1);
+		LongNumber result = 1;
+		while (p > 0)
+		{
+			if (p.IsOdd())
+				result = (result * base) % mod;
+			p = p / 2;
+			base = (base * base) % mod;
+		}
+		RemoveLeadingZeros();
+		return result;
 	}
-	else {
-		res = base;
+	else
+	{
+		LongNumber m = base.ModularExponentiation(p / 2, mod);
+		LongNumber res;
+		if (p.IsEven())
+		{
+			res = LongNumber(1);
+		}
+		else 
+		{
+			res = base;
+		}
+		LongNumber x = m * m;
+		return (x * res) % mod;
 	}
-	LongNumber x = m * m;
-	return (x * res) % mod;
-	//algo end
-	result.RemoveLeadingZeros();
-	return result;
 }
 
 LongNumber LongNumber::ModularInverse(const LongNumber & mod)
@@ -683,6 +725,59 @@ LongNumber LongNumber::ModularInverse(const LongNumber & mod)
 	return LongNumber();
 }
 
+LongNumber LongNumber::Exponentiation(const LongNumber & power)
+{
+	LongNumber base = *this;
+	LongNumber p = power;
+	LongNumber result = 1;
+	if (p == 1) {
+		return base;
+	}
+	if (p == 0) {
+		return LongNumber(1);
+	}
+	LongNumber m = base.Exponentiation(p / 2);
+	LongNumber res;
+	if (p.IsEven())
+	{
+		res = LongNumber(1);
+	}
+	else {
+		res = base;
+	}
+	LongNumber x = m * m;
+	x = x * res;
+	RemoveLeadingZeros();
+	return x;
+}
+
+LongNumber LongNumber::MulExponentent10(int power)
+{
+	LongNumber tmp = *this;
+	tmp.number.append(power,'0');
+	return tmp;
+}
+
+LongNumber LongNumber::DivExponentent10(int power)
+{
+	LongNumber tmp = *this;
+	int pos = tmp.number.size() - power;
+	if (pos <= 0)
+		return LongNumber(0);
+	tmp.number = tmp.number.substr(0, tmp.number.size() - power);
+	return tmp;
+}
+
+LongNumber LongNumber::ModExponentent10(int power)
+{
+	LongNumber tmp = *this;
+	int pos = tmp.number.size() - power;
+	if (pos < 0)
+		return tmp;
+	tmp.number = tmp.number.substr(tmp.number.size() - power);
+	return tmp;
+}
+
 LongNumber LongNumber::Absolute()
 {
 	return LongNumber(number);
@@ -692,13 +787,43 @@ LongNumber LongNumber::DivideBy2()
 {
 	LongNumber tmp = *this;
 	tmp = tmp * 5;
-	tmp.number = tmp.number.substr(0, tmp.number.size() - 1);
+	tmp = tmp.DivExponentent10(1);
 	return tmp;
 }
 
-bool LongNumber::IsPrime()
+LongNumber LongNumber::karatsuba(LongNumber & a, LongNumber & b)
 {
-	//TODO: Should Enhance Prime Check
+	if (a.number == "0" || b.number == "0")
+	{
+		return LongNumber(0);
+	}
+	if (a.number == "1")
+	{
+		return b;
+	}
+	if (b.number == "1")
+	{
+		return a;
+	}
+	int m = (a.number.size() > b.number.size()) ? a.number.size() : b.number.size();
+	if (m < 10)
+	{
+		return stoll(a.number) * stoll(b.number);
+	}
+	m = (m / 2) + (m % 2);
+	LongNumber x1 = a.DivExponentent10(m);
+	LongNumber x0 = a.ModExponentent10(m);
+	LongNumber y1 = b.DivExponentent10(m);
+	LongNumber y0 = b.ModExponentent10(m);
+	LongNumber u = karatsuba(x1, y1);
+	LongNumber v = karatsuba(x0, y0);
+	LongNumber w = (karatsuba((x1 + x0), (y1 + y0)) - u) - v;
+	return u.MulExponentent10(2*m) + w.MulExponentent10(m) + v;
+}
+
+bool LongNumber::IsPrime(int iterations)
+{
+	//TODO: Try to enhance IsPrime.
 	if (number == "2" || number == "3")
 		return true;
 	if (number == "0" || number == "1" || IsEven()) 
@@ -711,7 +836,7 @@ bool LongNumber::IsPrime()
 		q = q / 2;
 		k++;
 	}
-	for (int i = 0; i < 1; i++)
+	for (int i = 0; i < iterations; i++)
 	{
 		if (!MillerRabinTest(n, q, k))
 			return false;
@@ -753,7 +878,7 @@ void LongNumber::RemoveLeadingZeros()
 		number = "0";
 }
 
-bool LongNumber::MillerRabinTest(LongNumber n,LongNumber q,int k)
+bool LongNumber::MillerRabinTest(LongNumber& n,LongNumber& q, int k)
 {
 	random_device rseed;
 	mt19937 rgen(rseed());
